336df961f3b839c66cb0424253a8cd5fbea18ee7
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2016-01-18 10:56:44 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace MaruBatsuGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "たかしの○×ゲーム";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(99, 58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(186, 58);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(12, 144);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(99, 144);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(186, 144);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(12, 230);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(99, 230);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(186, 230);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(91, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 252);
+            this.label2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(178, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 252);
+            this.label3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(254, 10);
+            this.label4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(12, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(254, 10);
+            this.label5.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(343, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(299, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(172, 24);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "あなたの勝ちです";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 322);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media; // for SoundPlayer
+
+namespace MaruBatsuGame
+{
+    // マスデータを表す列挙型。
+    enum Masu
+    {
+        EMPTY,  // 空
+        MARU,   // マル (○)
+        BATSU   // バツ (×)
+    };
+
+    // 判定結果を表す列挙型。
+    enum HanteiKekka
+    {
+        MARU_KACHI,     // マルの勝ち。 
+        BATSU_KACHI,    // バツの勝ち。
+        HIKIWAKE        // 引き分け。
+    };
+
+    // 盤クラス。
+    class Board
+    {
+        // マスの総数。
+        public const int NUM_MASU = 3 * 3;
+        // 並びデータ。
+        public static int[][] narabi_data = new int[][] {
+            // 横の並び。
+            new int[]{1, 2, 3}, // #0
+            new int[]{4, 5, 6}, // #1
+            new int[]{7, 8, 9}, // #2
+            // 縦の並び。
+            new int[]{1, 4, 7}, // #3
+            new int[]{2, 5, 8}, // #4
+            new int[]{3, 6, 9}, // #5
+            // 斜めの並び。
+            new int[]{1, 5, 9}, // #6
+            new int[]{3, 5, 7}  // #7
+        };
+        // ビンゴデータ。並びインデックスからマスへの辞書。
+        // 例えば、4,5,6でマルが並んでいれば、bingo[1] == Masu.MARU になる。
+        public Dictionary<int, Masu> bingo;
+        // 数からマスへの辞書。
+        protected Dictionary<int, Masu> dict_number_kara_masu;
+
+        public Board()
+        {
+            // マスデータを作成する。
+            dict_number_kara_masu = new Dictionary<int, Masu>();
+            // ビンゴデータを作成する。
+            bingo = new Dictionary<int, Masu>();
+            // 初期化。
+            init();
+        }
+
+        // 盤の初期化。
+        public void init()
+        {
+            // マスデータを空にする。
+            for (int i = 1; i <= Board.NUM_MASU; ++i)
+            {
+                dict_number_kara_masu[i] = Masu.EMPTY;
+            }
+            // ビンゴデータをクリアする。
+            bingo.Clear();
+        }
+
+        // マスを取得する。
+        public Masu get_masu(int number)
+        {
+            if (dict_number_kara_masu.ContainsKey(number))
+            {
+                return dict_number_kara_masu[number];
+            }
+            return Masu.EMPTY;
+        }
+
+        // マスをセットする。
+        public void set_masu(int number, Masu masu)
+        {
+            dict_number_kara_masu[number] = masu;
+        }
+
+        // 判定。
+        public HanteiKekka hantei()
+        {
+            bingo.Clear();
+
+            HanteiKekka ret = HanteiKekka.HIKIWAKE;
+            for (int i = 0; i < narabi_data.Length; ++i)
+            {
+                var entry = narabi_data[i];
+                if (get_masu(entry[0]) == Masu.MARU &&
+                    get_masu(entry[1]) == Masu.MARU &&
+                    get_masu(entry[2]) == Masu.MARU)
+                {
+                    ret = HanteiKekka.MARU_KACHI;
+                    bingo[i] = Masu.MARU;
+                }
+                if (get_masu(entry[0]) == Masu.BATSU &&
+                    get_masu(entry[1]) == Masu.BATSU &&
+                    get_masu(entry[2]) == Masu.BATSU)
+                {
+                    ret = HanteiKekka.BATSU_KACHI;
+                    bingo[i] = Masu.BATSU;
+                }
+            }
+            return ret;
+        }
+    }
+
+    // メインフォーム。
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 盤。
+        Board board = new Board();
+        // 数からPictureBoxへの辞書。
+        Dictionary<int, PictureBox> dict_number_kara_pic;
+        // PictureBoxから数への辞書。
+        Dictionary<PictureBox, int> dict_pic_kara_number;
+        // 音。
+        SoundPlayer player_maru = new SoundPlayer(Properties.Resources.sound_maru);
+        SoundPlayer player_batsu = new SoundPlayer(Properties.Resources.sound_batsu);
+        SoundPlayer player_yatta = new SoundPlayer(Properties.Resources.sound_yatta);
+        SoundPlayer player_zannen = new SoundPlayer(Properties.Resources.sound_zannen);
+        // 点滅用のタイマー。
+        System.Timers.Timer timer_blink;
+        // コンピュータ待ちのタイマー。
+        System.Timers.Timer timer_computer;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 盤を初期化。
+            board.init();
+
+            // 数からPictureBoxへの辞書を作成する。
+            dict_number_kara_pic = new Dictionary<int, PictureBox>();
+            dict_number_kara_pic[1] = pictureBox1;
+            dict_number_kara_pic[2] = pictureBox2;
+            dict_number_kara_pic[3] = pictureBox3;
+            dict_number_kara_pic[4] = pictureBox4;
+            dict_number_kara_pic[5] = pictureBox5;
+            dict_number_kara_pic[6] = pictureBox6;
+            dict_number_kara_pic[7] = pictureBox7;
+            dict_number_kara_pic[8] = pictureBox8;
+            dict_number_kara_pic[9] = pictureBox9;
+
+            // PictureBoxから数への辞書を作成する。
+            dict_pic_kara_number = new Dictionary<PictureBox, int>();
+            dict_pic_kara_number[pictureBox1] = 1;
+            dict_pic_kara_number[pictureBox2] = 2;
+            dict_pic_kara_number[pictureBox3] = 3;
+            dict_pic_kara_number[pictureBox4] = 4;
+            dict_pic_kara_number[pictureBox5] = 5;
+            dict_pic_kara_number[pictureBox6] = 6;
+            dict_pic_kara_number[pictureBox7] = 7;
+            dict_pic_kara_number[pictureBox8] = 8;
+            dict_pic_kara_number[pictureBox9] = 9;
+
+            // 点滅用のタイマーを作成する。
+            timer_blink = new System.Timers.Timer();
+            timer_blink.Interval = 1000;
+
+            // コンピュータ待ちのタイマーを作成する。
+            timer_computer = new System.Timers.Timer();
+            timer_computer.Interval = 2000;
+        }
+
+        // マスがクリックされた。
+        void on_masu_click(int number)
+        {
+            // すでにマスが置かれているか確認。
+            if (board.get_masu(number) != Masu.EMPTY)
+            {
+                return;
+            }
+
+            // 盤のマスを変える。
+            board.set_masu(number, Masu.MARU);
+
+            // マスの画像を変える。
+            dict_number_kara_pic[number].Image = Properties.Resources.image_maru;
+
+            // 判定する。
+            var kekka = board.hantei();
+            switch (kekka)
+            {
+                case HanteiKekka.MARU_KACHI:
+                    player_yatta.Play();
+                    break;
+                case HanteiKekka.BATSU_KACHI:
+                    player_zannen.Play();
+                    break;
+                case HanteiKekka.HIKIWAKE:
+                    player_maru.Play();
+                    break;
+            }
+        }
+
+        // 「開始」ボタンがクリックされた。
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 盤を初期化する。
+            board.init();
+
+            // PicutureBoxの画像をクリアする。
+            for (int i = 1; i <= Board.NUM_MASU; ++i)
+            {
+                dict_number_kara_pic[i].Image = null;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            on_masu_click(1);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            on_masu_click(2);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            on_masu_click(3);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            on_masu_click(4);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            on_masu_click(5);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            on_masu_click(6);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            on_masu_click(7);
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            on_masu_click(8);
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            on_masu_click(9);
+        }
+    }
+}
